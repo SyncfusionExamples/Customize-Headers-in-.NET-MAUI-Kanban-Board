e8162fce33b73a219c6fddeaf01c0310dc9fd925
tineshkumar.sf4652 <tineshkumar.sf4652@gmail.com>
2024-11-26 18:32:13 +0530

    update the header customization of kanban kb sample

---
+++ HeaderCustomization/HeaderCustomization/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HeaderCustomization
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HeaderCustomization/HeaderCustomization/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace HeaderCustomization
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
